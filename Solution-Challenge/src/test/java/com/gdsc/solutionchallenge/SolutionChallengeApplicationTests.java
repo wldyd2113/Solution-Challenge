7c88fa917c68e8bd72558c90657ac55a7fff0b18
Kim, Seokwan <sukwan1106@gmail.com>
2023-12-19 16:21:36 +0900

    first commit

---
+++ Solution-Challenge/src/test/java/com/gdsc/solutionchallenge/SolutionChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.solutionchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
