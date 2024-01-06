cb15869ca8294340807f3dc2b2778f5523d42a06
Lee, Giyeon <leessjjgg123456@naver.com>
2024-01-06 17:17:54 +0900

    오류 수정 완료

--- Solution_Challenge/src/main/java/gdsc/jwt/JwtAuthenticationEntryPoint.java
+++ Solution_Challenge/src/main/java/gdsc/jwt/JwtAuthenticationEntryPoint.java
@@ -9,11 +9,15 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+// JwtAuthenticationEntryPoint 클래스는 Spring Security에서 인증되지 않은 사용자가 보호된 리소스에 액세스하려고 할 때 호출되는 핸들러를 정의합니다.
+
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    // AuthenticationEntryPoint 인터페이스의 메소드를 구현합니다.
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException {
+        // 인증되지 않은 사용자가 보호된 리소스에 액세스하려고 할 때, HTTP 응답에 상태 코드 401(Unauthorized)을 설정합니다.
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
--- Solution_Challenge/src/main/java/gdsc/service/CustomUserDetailsService.java
+++ Solution_Challenge/src/main/java/gdsc/service/CustomUserDetailsService.java
@@ -16,25 +16,33 @@ import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
+// CustomUserDetailsService 클래스는 Spring Security의 UserDetailsService를 구현하여 사용자 정보를 로드하는 역할을 합니다.
+
 public class CustomUserDetailsService implements UserDetailsService {
 
+    // UserRepository를 주입받아 사용자 정보를 조회하는 데 사용합니다.
     private final UserRepository userRepository;
 
+    // 주입받은 UserRepository를 이용하여 사용자 정보를 조회하는 메소드입니다.
     @Override
     @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 사용자 이메일을 기반으로 UserRepository에서 사용자 정보를 조회합니다.
         return userRepository.findByEmail(username)
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException(username + "-> DB에서 찾을 수 없습니다."));
     }
 
-    private UserDetails createUserDetails(User user){
+    // 사용자 정보를 기반으로 UserDetails 객체를 생성하는 메소드입니다.
+    private UserDetails createUserDetails(User user) {
+        // 사용자의 권한을 Spring Security의 GrantedAuthority로 변환합니다.
         GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getRole().toString());
 
+        // UserDetails 인터페이스를 구현한 객체를 생성하여 반환합니다.
         return new org.springframework.security.core.userdetails.User(
-                String.valueOf(user.getId()),
-                user.getPassword(),
-                Collections.singleton(grantedAuthority)
+                String.valueOf(user.getId()),  // 사용자의 ID를 문자열로 변환하여 사용합니다.
+                user.getPassword(),  // 사용자의 비밀번호를 사용합니다.
+                Collections.singleton(grantedAuthority)  // 사용자의 권한을 담은 Set을 사용합니다.
         );
     }
 }
