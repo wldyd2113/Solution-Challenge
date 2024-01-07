519929a1af825a9c5117fee6629ec2651044ddd6
Kim, Seokwan <sukwan1106@gmail.com>
2024-01-07 21:47:17 +0900

    주석

--- Solution-Challenge/src/main/java/com/gdsc/solutionchallenge/service/UserService.java
+++ Solution-Challenge/src/main/java/com/gdsc/solutionchallenge/service/UserService.java
@@ -97,12 +97,10 @@ public class UserService {
 
         User user = userRepository.findById(Long.valueOf(userEmail))
                 .orElseThrow(() -> new RuntimeException("현재 로그인한 사용자를 찾을 수 없습니다."));
-
         if(!passwordEncoder.matches(currentPassword, user.getPassword())){
             throw new RuntimeException("현재 비밀번호가 일치하지 않습니다.");
         }
         user.setPassword(passwordEncoder.encode(newPassword));
         userRepository.save(user);
-//        test
     }
 }
