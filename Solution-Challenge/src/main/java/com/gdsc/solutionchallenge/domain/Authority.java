2f78753d5c0d17296ce5b21017641588afe52071
Kim, Seokwan <sukwan1106@gmail.com>
2024-01-07 22:52:59 +0900

    수정

--- Solution-Challenge/src/main/java/com/gdsc/solutionchallenge/domain/Authority.java
+++
@@ -1,6 +0,0 @@
-package com.gdsc.solutionchallenge.domain;
-
-public enum Authority {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- Solution-Challenge/src/main/java/com/gdsc/solutionchallenge/service/CustomUserDetailsService.java
+++ Solution-Challenge/src/main/java/com/gdsc/solutionchallenge/service/CustomUserDetailsService.java
@@ -1,38 +1,38 @@
-package com.gdsc.solutionchallenge.service;
-
-import com.gdsc.solutionchallenge.domain.User;
-import com.gdsc.solutionchallenge.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collections;
-
-@Service
-@RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
-
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-        return userRepository.findByEmail(username)
-                .map(this::createUserDetails)
-                .orElseThrow(() -> new UsernameNotFoundException(username + " -> DB에서 찾을 수 없습니다."));
-    }
-    private UserDetails createUserDetails(User user){
-        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getAuthority().toString());
-
-        return new org.springframework.security.core.userdetails.User(
-                String.valueOf(user.getId()),
-                user.getPassword(),
-                Collections.singleton(grantedAuthority)
-        );
-    }
-
-}
+//package com.gdsc.solutionchallenge.service;
+//
+//import com.gdsc.solutionchallenge.domain.User;
+//import com.gdsc.solutionchallenge.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.Collections;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CustomUserDetailsService implements UserDetailsService {
+//    private final UserRepository userRepository;
+//
+//    @Override
+//    @Transactional
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+//        return userRepository.findByEmail(username)
+//                .map(this::createUserDetails)
+//                .orElseThrow(() -> new UsernameNotFoundException(username + " -> DB에서 찾을 수 없습니다."));
+//    }
+//    private UserDetails createUserDetails(User user){
+//        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getAuthority().toString());
+//
+//        return new org.springframework.security.core.userdetails.User(
+//                String.valueOf(user.getId()),
+//                user.getPassword(),
+//                Collections.singleton(grantedAuthority)
+//        );
+//    }
+//
+//}
