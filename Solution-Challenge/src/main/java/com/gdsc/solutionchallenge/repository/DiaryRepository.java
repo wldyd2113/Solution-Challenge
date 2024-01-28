746399073f34521f65c7c30ae01c20040c3e38dc
Kim, Seokwan <sukwan1106@gmail.com>
2024-01-28 22:21:30 +0900

    오래된 일기 조회 본인 제회

--- Solution-Challenge/src/main/java/com/gdsc/solutionchallenge/repository/DiaryRepository.java
+++ Solution-Challenge/src/main/java/com/gdsc/solutionchallenge/repository/DiaryRepository.java
@@ -4,13 +4,14 @@ import com.gdsc.solutionchallenge.domain.Diary;
 import com.gdsc.solutionchallenge.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-    @Query("SELECT d FROM Diary d WHERE d.shareDiary IS NOT NULL AND d.isViewed = false ORDER BY d.createdAt ASC LIMIT 1")
-    Optional<Diary> findOldestDiary();
+    @Query("SELECT d FROM Diary d WHERE d.shareDiary IS NOT NULL AND d.isViewed = false AND d.user <> :loggedInUser ORDER BY d.createdAt ASC LIMIT 1")
+    Optional<Diary> findOldestDiary(@Param("loggedInUser") User loggedInUser);
 
     Optional<Diary> findByDateAndUser(String date, User user);
 
